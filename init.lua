bc1dfa01257a2c760624c1b954e10001251aaeb2
vybukhivka <99396589+vybukhivka@users.noreply.github.com>
2024-09-15 10:36:23 +0300

    highlight on yank autocomand

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
 require("main.settings.set")
 require("main.settings.remap")
 require("main.lazy")
+
+-- Autocomands
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('yank-highlight', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  desc = 'Highlight on yank',
+})
+
--- lua/main/lazy.lua
+++ lua/main/lazy.lua
@@ -1,4 +1,3 @@
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
