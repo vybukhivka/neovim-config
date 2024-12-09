83d0af554dbb5baeb5ae74ee2e17368455f946a0
vybukhivka <viktorbakhovka@gmail.com>
2024-12-09 16:53:21 +0200

    colors update, ai tools removed

--- lua/main/plugins/colorsheme.lua
+++ lua/main/plugins/colorsheme.lua
@@ -11,7 +11,7 @@ return {
 							bg_p2 = "#1f1c1c",
 							bg_dim = "#1f1e1e",
 							pmenu = {
-								bg = "#1f1e1e",
+								bg = "#1b1818",
 								bg_sel = "#393836",
 								bg_thumb = "#282727",
 								bg_sbar = "#1D1C19"
@@ -19,6 +19,25 @@ return {
 							float = {
 								bg = "#1a1919",
 							}
+						},
+						syn = {
+							string = "#4b8b8b",
+							constant = "#f4ecec",
+							number = "#b4593b",
+							identifier = "#ca4848",
+							fun = "#a06d3a",
+							statement = "#ca4848",
+							operator = "#6f6fc5",
+							preproc = "#7272ca",
+							type = "#a06d3a",
+							regex = "#4b8b8b",
+							deprecated = "#00ff00",
+							punct = "#6e6767",
+							special1 = "#7272ca",
+							special2 = "#f4ecec",
+							special3 = "#7272ca",
+							-- type = "#00ff00",
+							-- operator = "#56548b"
 						}
 					}
 				}
