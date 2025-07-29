92136a6bbbb0209b5f070af82627b945d91305ed
vybukhivka <viktorbakhovka@gmail.com>
2025-07-29 22:51:06 +0300

    harpoon remap

--- lua/main/plugins/harpoon.lua
+++ lua/main/plugins/harpoon.lua
@@ -8,10 +8,10 @@ return {
 		keymap.set("n", "<leader>a", mark.add_file)
 		keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-		keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-		keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-		keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-		keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+		keymap.set("n", "<C-t>", function() ui.nav_file(1) end)
+		keymap.set("n", "<C-y>", function() ui.nav_file(2) end)
+		keymap.set("n", "<C-h>", function() ui.nav_file(3) end)
+		keymap.set("n", "<C-n>", function() ui.nav_file(4) end)
 	end
 }
 
