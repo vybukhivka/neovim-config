fae93558525b3f0fd49b2f7f32bf1e03151e28dc
vybukhivka <viktorbakhovka@gmail.com>
2025-11-29 12:15:18 +0200

    theme: fahrenheit

--- lua/main/plugins/colorsheme.lua
+++ lua/main/plugins/colorsheme.lua
@@ -1,51 +1,15 @@
 return {
-	'rebelot/kanagawa.nvim',
+	'fcpg/vim-fahrenheit',
 	priority = 1000,
-	config = function()
-		require('kanagawa').setup({
-			colors = {
-				theme = {
-					all = {
-						ui = {
-							bg_gutter = "none",
-							bg_p2 = "#1f1c1c",
-							bg_dim = "#1f1e1e",
-							pmenu = {
-								bg = "#1b1818",
-								bg_sel = "#393836",
-								bg_thumb = "#282727",
-								bg_sbar = "#1D1C19"
-							},
-							float = {
-								bg = "#1a1919",
-							}
-						},
-						syn = {
-							string = "#4b8b8b",
-							constant = "#f4ecec",
-							number = "#b4593b",
-							identifier = "#ca4848",
-							fun = "#a06d3a",
-							statement = "#ca4848",
-							operator = "#6f6fc5",
-							preproc = "#7272ca",
-							type = "#a06d3a",
-							regex = "#4b8b8b",
-							deprecated = "#00ff00",
-							punct = "#6e6767",
-							special1 = "#7272ca",
-							special2 = "#f4ecec",
-							special3 = "#7272ca",
-							-- type = "#00ff00",
-							-- operator = "#56548b"
-						}
-					}
-				}
-			}
-		})
-	end,
 	init = function()
-		vim.cmd 'colorscheme kanagawa-dragon'
+		vim.opt.termguicolors = true
+		vim.cmd 'colorscheme fahrenheit'
+		vim.cmd [[
+			highlight Visual guibg=#282422 guifg=NONE
+		]]
+		vim.cmd [[
+			highlight CursorLine guibg=#111111 guifg=NONE
+		]]
+
 	end
 }
-
