6e0cd51eaba20b81c59da6d3157a55b851b6da52
vybukhivka <viktorbakhovka@gmail.com>
2025-11-29 12:19:34 +0200

    lsp suggestions max results 5 removed

--- lua/main/plugins/cmp.lua
+++ lua/main/plugins/cmp.lua
@@ -69,9 +69,9 @@ return {
 				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 			},
 			sources = {
-				{ name = 'nvim_lsp', max_item_count = 5 },
-				{ name = 'luasnip', max_item_count = 5 },
-				{ name = 'path', max_item_count = 5 },
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' },
+				{ name = 'path' },
 			},
 		}
 	end,
