5cd57e984932ce6596a23e89591f409c4b16e15d
vybukhivka <viktorbakhovka@gmail.com>
2025-12-05 00:22:47 +0200

    kanagawa with colors

--- lua/main/plugins/colorsheme.lua
+++ lua/main/plugins/colorsheme.lua
@@ -1,15 +1,42 @@
 return {
-	'fcpg/vim-fahrenheit',
+	'rebelot/kanagawa.nvim',
 	priority = 1000,
 	init = function()
-		vim.opt.termguicolors = true
-		vim.cmd 'colorscheme fahrenheit'
-		vim.cmd [[
-			highlight Visual guibg=#282422 guifg=NONE
-		]]
-		vim.cmd [[
-			highlight CursorLine guibg=#111111 guifg=NONE
-		]]
+		-- Default options:
+		require('kanagawa').setup({
+			compile = false,             -- enable compiling the colorscheme
+			undercurl = true,            -- enable undercurls
+			commentStyle = { italic = false },
+			functionStyle = {},
+			keywordStyle = { italic = false},
+			statementStyle = { bold = true },
+			typeStyle = {},
+			
+			transparent = false,         -- do not set background color
+			dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+			terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+			colors = {                   -- add/modify theme and palette colors
+				palette = {
+					waveBlue1 = "#2A2A37";
+				},
+				theme = { wave = {}, lotus = {}, dragon = {
+					ui = { 
+						bg_gutter = "#181616",
+						float = {
+							bg = "#121212",
+						},
+						pmenu = {
+							bg = "#121212",
+						},
+					},
+				}, all = {} },
+			},
+			overrides = function(colors) -- add/modify highlights
+				return {}
+			end,
+		})
 
+		-- setup must be called before loading
+		vim.cmd("colorscheme kanagawa-dragon")
 	end
 }
